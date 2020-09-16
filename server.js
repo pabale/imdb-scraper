37b94149027088286ab8a2bc6f2cba721e6a0918
sagar <sagar.pabale@letsallhelp.org>
2020-09-16 13:58:23 +0530

    ok

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ db.sequelize.sync(); //insert database
 
 
 
-/*function file_chunk(filename) 
+function file_chunk(filename) 
 {
 	  var stream;
-	  var file = './tsvfile/'+filename+'.tsv';
+	  var file = './tsvfile/'+filename;
 	  var linesCount = 0;
-	  var i=1;
+	  /*var i=1;
 
 	  var dir = './xyz/'+filename;
 	  var dir1 = './xyz/';
@@ -32,7 +32,7 @@ db.sequelize.sync(); //insert database
 
 		if (!fs.existsSync(dir1)){
 	    	fs.mkdirSync(dir1);
-		}
+		}*/
 	  
 	  var rl = readline.createInterface({
 	      input: fs.createReadStream(file),
@@ -41,11 +41,11 @@ db.sequelize.sync(); //insert database
 	  });
 
 	  
-	  stream = fs.createWriteStream(dir+"/"+filename+"1.txt");
+	 /* stream = fs.createWriteStream(dir+"/"+filename+"1.txt");*/
 	  rl.on('line', function (line) {
 	      linesCount++; // on each linebreak, add +1 to 'linesCount'
 	      
-	      if(linesCount%400000==0) {
+	      /*if(linesCount%400000==0) {
 	        stream.write(line);
 	        i++;
 	        //console.log(filename+" : "+i); 
@@ -53,14 +53,22 @@ db.sequelize.sync(); //insert database
 	        stream = fs.createWriteStream(dir+"/"+filename+i+".txt");
 	      }else{
 	        stream.write(line+"\r\n");  
-	      }
+	      }*/
 	  });
 
 	  rl.on('close', function () {
-	    stream.end();
+	    //stream.end();
 	    console.log(filename+" : "+linesCount)
 	  }); 
-}*/
+}
+
+file_chunk('name.basics.tsv');
+file_chunk('title.akas.tsv');
+file_chunk('title.basics.tsv');
+file_chunk('title.crew.tsv');
+file_chunk('title.episode.tsv');
+file_chunk('title.principals.tsv');
+file_chunk('title.ratings.tsv');
  
 
 
