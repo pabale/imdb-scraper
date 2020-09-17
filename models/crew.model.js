15bdb781aed44ee97dde5216dfdb67de3663dd12
sagar <sagar.pabale@letsallhelp.org>
2020-09-17 06:59:56 +0530

    ok

--- models/crew.model.js
+++ models/crew.model.js
@@ -2,8 +2,6 @@ module.exports = (sequelize, Sequelize) => {
   const Crew = sequelize.define("crew", {
     tconst: {
       type: Sequelize.STRING(100),
-      allowNull: false,
-      unique: true
     },
     directors: {
       type: Sequelize.TEXT
