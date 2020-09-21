4c3c78f6b185b9b2fc8abfba1db65411f6bbc1ac
sagar <sagar.pabale@letsallhelp.org>
2020-09-21 11:35:53 +0530

    ok

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const express = require("express");
 
 var app = express();
 
-cron.schedule("49 26 11 * * *", function() {
+cron.schedule("50 6 2 * * *", function() {
     console.log("Running Cron Job");
     require("./controllers/rating.controller.js");
 });
 
-cron.schedule("30 23 11 * * *", function() {
+cron.schedule("30 8 2 * * *", function() {
     console.log("Running Cron Job");
     require("./controllers/titlebasicscontroller.js");
 });
