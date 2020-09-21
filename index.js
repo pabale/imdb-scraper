0acaaac61c7aad4f0e2a18bd4770d1b384b2767d
sagar <sagar.pabale@letsallhelp.org>
2020-09-21 11:30:15 +0530

    ok

--- index.js
+++ index.js
@@ -1,63 +1,17 @@
 
-const request = require('request');
-const lib = require('./lib');
+const cron = require("node-cron");
+const express = require("express");
 
-const options = {
-    method: 'GET',
-    uri: `https://www.imdb.com/search/title/?title_type=tv_series,tv_miniseries&release_date=2015-01-01,&user_rating=6.0,&num_votes=50,&count=250&view=advanced`,
-  }
+var app = express();
 
-async function scrap_request(options) {
-	await request.get(options, (err, res, body) => {
-	    lib.parse_data(body,err);
-	 });
-}
+cron.schedule("49 26 11 * * *", function() {
+    console.log("Running Cron Job");
+    require("./controllers/rating.controller.js");
+});
 
+cron.schedule("30 23 11 * * *", function() {
+    console.log("Running Cron Job");
+    require("./controllers/titlebasicscontroller.js");
+});
 
-scrap_request(options);  //first page url
-
-//scrape every pages from the url
-for(var k=251;k<=6787;k=k+250){
-	const options = {
-    method: 'GET',
-    uri: `https://www.imdb.com/search/title/?title_type=tv_series,tv_miniseries&release_date=2015-01-01,&user_rating=6.0,&num_votes=50,&count=250&start=`+k+`&ref_=adv_nxt`,
-    json: true
-  }
-  scrap_request(options);
-}
-
-
-   
-
-
-
-
-
-  var stream;
-  var file = './tsvfile/title.principals.tsv';
-  var linesCount = 0;
-  var i=1;
-  
-  var rl = readline.createInterface({
-      input: fs.createReadStream(file),
-      output: process.stdout,
-      terminal: false
-  });
-
-  var stream = fs.createWriteStream("copy1.txt");
-  rl.on('line', function (line) {
-      linesCount++; // on each linebreak, add +1 to 'linesCount'
-      
-      if(linesCount==10000) {
-        stream.write(line);
-        i++;
-        stream.end();
-        var stream = fs.createWriteStream("copy"+i+".txt");
-      }else{
-        stream.write(line);  
-      }
-  });
-  rl.on('close', function () {
-    stream.end();
-    
-  });
+app.listen("8080");
