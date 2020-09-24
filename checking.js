6587ac4b7af90c2cef60d79e4504ec5168ef7737
sagar <sagar.pabale@letsallhelp.org>
2020-09-24 11:13:42 +0530

    ok

--- checking.js
+++ checking.js
@@ -2,17 +2,21 @@
 const cron = require("node-cron");
 const express = require("express");
 
-var app = express();
-var datetime;
+//var app = express();
+//var datetime;
 
 cron.schedule("1 1 1 * * *", function() {
-    datetime = new Date();
-    console.log(datetime);
-    
+  console.log("night 1");
 });
 
-cron.schedule("1 30 1 * * *", function() {
-    datetime = new Date();
-    console.log(datetime);
-    
+cron.schedule("1 2 1 * * *", function() {
+   console.log("night 1");
+});
+
+cron.schedule("1 1 13 * * *", function() {
+	console.log("afternoon 1");
+});
+
+cron.schedule("1 2 13 * * *", function() {
+	console.log("afternoon 1");
 });
